12303b1b11f87fcfb69731d8e912082358527bd2
Rosie O'Donnell <rosie.odonnell@talk21.com>
2021-03-02 17:40:26 +0000

    getting timer to go directly from 25:00 to 5:00 at end

--- timer.js
+++ timer.js
@@ -19,7 +19,7 @@ let workSession = startTime(1500);
 
 //MY EVENT LISTENERS:
 
-timeRemaining.innerHTML = startTime(300);
+timeRemaining.innerHTML = startTime(1500);
 
 start.addEventListener("click", countdown);
 startBreak.addEventListener("click", timer5Function);
@@ -27,7 +27,7 @@ restart.addEventListener("click", timer25Function);
 pause.addEventListener("click", pauseButton);
 
 
-//   MY FUNCTION FOR ADDING A ZERO IN FRONT 
+//   MY FUNCTION FOR ADDING A ZERO IN FRONT OF SINGLE DIGITS
 function addZero() {
 
   if (minutes < 10 && seconds <10) {
@@ -37,17 +37,11 @@ function addZero() {
   } else { if(seconds < 10 ) {
     return `${minutes}:0${seconds}`; 
   }
-       else {
+    else {
     return `${minutes}:${seconds}`; 
-       }
-      } }
-
-  // if (minutes < 10) {
-  //   minutes = `0${minutes}`;
-  // } 
-  //     if (seconds < 10) {
-  //     seconds = `0${seconds}`;
-  //     }  
+      }
+    } 
+  }  
 }
 
 
@@ -55,62 +49,49 @@ function addZero() {
 function startTime(time) {
   minutes = Math.floor(time / 60);
   seconds = time % 60;
-
-  return addZero(); // I have moved the add zero instructions to a separate function, so I can reuse.
-      
+  return addZero();    
 }
 
-// FUNCTION FOR MY 5 MINUTE COUNTDOWN
+// FUNCTION FOR MY 5 MINUTE COUNTDOWN:
 
 function timer5Function() {
   is5ClockTicking = true;
-  startTime(30);
+  startTime(300);
 
-  setInterval
-      (function() { if(seconds > 0) {
+  setInterval(function() { 
+        if(seconds > 0) {
           seconds--;
-      } else if(minutes > 0 && seconds <= 0) {
-          seconds = 59;
-          minutes--;
+          } else if(minutes > 0 && seconds <= 0) {
+            seconds = 59;
+            minutes--;
        }
+       
+    timeRemaining.innerText = addZero();
 
-       timeRemaining.innerText = addZero();
-
-    //    if (minutes < 10) {
-    // timeRemaining.innerText = `0${minutes}:${seconds}`; 
-
-    //    }
-    //    else {
-    // timeRemaining.innerText = `${minutes}:${seconds}`; 
-    //    }
-
-
-  if (minutes == 0 && seconds ==0) {
-  clearInterval(timer5Function);
-  is5ClockTicking = false;
-  }
-  console.log(is5ClockTicking)
-}, 
-      1000)
-
+      if (minutes == 0 && seconds ==0) {
+          timer25Function();
+          clearInterval(timer5Function);
+          is5ClockTicking = false;
+          } //console.log(is5ClockTicking)
+  }, 1000)
 }
 
 // FUNCTION FOR MY 25 MINUTE COUNTDOWN
 
-    function timer25Function() {
-      is25ClockTicking = true;
-      startTime(1500);
+function timer25Function() {
+  is25ClockTicking = true;
+  startTime(1500);
     
-      setInterval
-          (function() { if(seconds > 0) {
-              seconds--;
+  setInterval(function() { 
+        if(seconds > 0) {
+          seconds--;
           } else if(minutes > 0 && seconds <= 0) {
               seconds = 59;
               minutes--;
            }
 
           
-           //timeRemaining.innerText = addZero();
+          timeRemaining.innerText = addZero();
     
       if (minutes <= 0 && seconds <=0) {
       clearInterval(timer25Function);
@@ -127,11 +108,12 @@ function timer5Function() {
 // CALLBACK FUNCTION FOR 25 MINUTE COUNTDOWN
 // THIS WILL BE MY FUNCTION SO THAT THE 25 MINUTE COUNTDOWN TRIGGERS THE 5 MINUTE COUNTDOWN AT 00:00
 
-function countdown(timer5Function) {
+function countdown() {
   is25ClockTicking = true;
-  startTime(5);
+  startTime(1500);
 
-  setInterval(function() { 
+  // first function
+  var timing = setInterval(function() { 
       if(seconds > 0) {
           seconds--;
       } else if(minutes > 0 && seconds <= 0) {
@@ -139,35 +121,34 @@ function countdown(timer5Function) {
           minutes--;
        }
       
-  timeRemaining.innerText = `${minutes}:${seconds}`;
+       timeRemaining.innerText = addZero();
 
   if (minutes <= 0 && seconds <=0) {
-  clearInterval(timer25Function);
+  timer5Function();
+  clearInterval(timing);
   is25ClockTicking = false;
-    // HERE I WANT TO SEND YOU TO THE 5 MINUTE FUNCTION, BUT IT EXCEEDS CALLSTACK
+    
   }
   console.log(is25ClockTicking)
 }, 
       1000);
-// The issue is that the above function isn't ending, which is why this function below isn't working
-timer5Function();   
 console.log(timer5Function);  
 }
-//timer5Function();
-//}
- 
-
-
 
 //MY PAUSE BUTTON
 
-function pauseButton() {
-  if (is25ClockTicking) {
-  clearInterval(timer25Function)
-} else {
-  clearInterval(timer5Function)
-}
-}
+// function pauseButton() {
+//   if (is25ClockTicking) {
+//   clearInterval(timer25Function)
+//     is25ClockTicking = false;
+//   }
+//     if(!is25ClockTicking) {
+//   timer25Function();
+//     }
+// } else {
+//   clearInterval(timer5Function)
+// }
+// }
 
 
 
